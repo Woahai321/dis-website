42aaf861ed3c212649d59deb979693d62238c308
Aid <woahai.com@gmail.com>
2023-01-13 02:13:21 +0000

    main

---
+++ constants/data.js
@@ -0,0 +1,117 @@
+const userData = {
+    githubUsername: "Woahai321",
+  name: "amunns",
+  designation: "Technical CSE",
+  avatarUrl: "/avatar.png",
+  email: "arhmunns@gmail.com",
+  phone: "+not for now",
+  address: "Hertfordshire, United Kingdom",
+  projects: [
+    {
+      title: "WoahAI",
+      link: "https://woahai.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "ATPS",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+        title: "MQL4",
+        link: "https://www.mql4.com/",
+      imgUrl: "/portfolio.png",
+    },
+    {
+        title: "Woah Labs",
+      link: "https://labs.woahai.com",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+        title: "Woah Gallery",
+      link: "https://gallery.woahai.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+        title: "Coming Soon",
+      link: "https://amunns.co.uk",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Coming Soon",
+      link: "https://amunns.co.uk",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+        title: "Coming Soon",
+      link: "https://amunns.co.uk",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a project manager and aspiring coder learning new skills and making projects to document the journey, tag long for the ride",
+    description: [
+      `As a technical support specialist with a diverse background in both the corporate and education sectors, I bring a unique set of skills to the table. With a proven track record of being able to diagnose and troubleshoot issues remotely, I pride myself on being able to provide effective solutions for a wide range of technical problems.`,
+      `My experience in both customer and internal support, as well as working with a variety of software and hardware, has given me the flexibility to handle any technical support challenge that comes my way. I am not only comfortable working with the likes of Active Directory, ConnectWise, and Microsoft System Center Configuration Manager, but I also thrive in it. Whether it's taking on Active Directory admin tasks, imaging hardware, or maintaining Service-Level Agreements and Key Performance Indicators, I am always up for a challenge.`,
+      `I am also able to work with children, which has been a valuable skill that I honed while working in an educational environment. I am patient, understanding, and always willing to go the extra mile to help make technology accessible to everyone, regardless of their age or experience level. `,
+    ],
+    currentProject: "WoahAI",
+    currentProjectUrl: "https://woahai.com",
+  },
+  experience: [
+    {
+      title: "Co-Founder",
+      company: "WoahAi & ATPS",
+      year: "Current Hustles",
+      companyLink: "https://woahai.com",
+            desc: "At WoahAI, our mission is to make the power of artificial intelligence easily accessible to everyone. Our company was founded by myself and my co-founder Ryan with the goal of bringing the beauty and capability of AI to the fingertips of our customers. We have achieved this by creating tens of thousands of custom, highly-detailed and beautifully curated images. We have a deep understanding and expertise in the field of Artificial Intelligence and we are constantly researching and experimenting with new tools and techniques to improve our offerings.We understand the importance of staying up- to - date with the latest advancements in AI and we are passionate about staying ahead of the curve to provide our customers with the best possible results!",
+    },
+    {
+      title: "Project Managment",
+      company: "Defense.com & Bulletproof",
+      year: "2022 - 2023",
+        companyLink: "https://www.defense.com/",
+        desc: "As a customer success and technical support representative for a cyber security SaaS platform, my main responsibilities include ensuring that our customers are effectively utilizing our software and addressing any technical issues or concerns that they may have. This includes providing troubleshooting assistance, answering product-related questions, and working with our development team to resolve any bugs or glitches in the platform.In addition to customer success and technical support, I also play a key role in managing projects related to Penetration Testing, SIEM, and DPO(Data Protection Officer) for our business.This includes coordinating with our internal teams and external partners to ensure that these projects are completed on time and within budget, and that they meet the specific requirements and expectations of our customers.This also includes monitoring progress, identifying and addressing any issues that arise, and communicating regularly with stakeholders to keep them informed of progress and any potential risks or delays",
+    },
+    {
+      title: "Tech Support",
+      company: "Motocaddy",
+      year: "2021-2022",
+        companyLink: "https://www.motocaddy.com",
+        desc: "As a technical support representative, my main responsibilities include providing assistance to customers, service agents and internal staff through various forms of communication such as phone, in-person, email and live chat. My role involved working with a diverse range of customers, including direct customers and a network of service agents across the UK. I was also responsible for providing remote diagnosis and troubleshooting for a wide range of electric golf trolleys and accessories, dating back to 2008. My knowledge of the product catalogue, consisting of over 25 electric golf trolleys and 30+ accessories, allowed me to quickly and effectively diagnose issues and provide solutions.I was able to achieve this within 6 months of joining the company, which demonstrated my ability to quickly adapt and learn the products and their technical aspects, and provide the highest level of customer service by being proactive, responsive, and helpful!",
+    },
+    {
+        title: "First Job & Apprenticeship",
+        company: "Everest Home Improvement",
+      year: "2018 - 2020",
+      companyLink: "https://everest.co.uk",
+            desc: "During my time at Everest, I was exposed to a wide variety of tools and techniques that were required for my job role as IT Support Engineer. Some of the key tools and technologies that I became proficient in include ConnectWise (a ticketing system), Asset Manager, Microsoft System Center Configuration Manager, Active Directory, Webroot SecureAnywhere (Anti-Virus Admin) and many others. My daily responsibilities as an IT Support Engineer included a variety of tasks such as Active Directory Administration(Starter/ Leaver / Edit), first - line activities, software and hardware related, which were conducted both remotely and in person.I was also responsible for imaging hardware for sales staff and ensuring that SLA & KPI standards were kept by practicing good ticket management and escalation protocols.As part of my role, I was responsible for handling software and hardware related issues, troubleshooting and resolving technical problems, and providing guidance and training to other team members!",
+    },
+    {
+      title: "College",
+      company: "NHC, Herts",
+      year: "2016 - 2018",
+        companyLink: "https://www.nhc.ac.uk/",
+            desc: "I studied a Level 3 Extended Diploma in Business and passed with a DDD* grade. This 2-year course provided me with a comprehensive understanding of the foundations of business. The course covered a wide range of topics, such as Exploring Business, Developing a Marketing Campaign, Personal and Business Finance, Managing an Event, International Business, Principals of Management, Business Decision Making, Recruitment, Team Building in Business, Investigating Customer Service, Investigating Retail Business, Visual Merchandising, Digital/Online Marketing, Running a Promotion, Pitching a New Business, Investigating Corporate Social Responsibility, Work Experience in Business, and Supply Chain/Procurement. Throughout this course, I gained a solid understanding of the different aspects of business, from the basics of marketing and finance to more advanced topics such as international business, digital marketing, and supply chain management.I was able to learn about the various strategies and techniques used in business, as well as the various tools and technologies that are used to support decision- making and operations.Additionally, I gained practical experience through various projects, such as developing marketing campaigns, managing events, and running a promotion, which helped me to apply the knowledge I gained in a real - world setting.Overall, this course has provided me with a solid foundation in business and has prepared me well for further education or a career in this field!",
+    },
+    {
+      title: "School",
+        company: "Kingshott, Herts",
+        year: "2010 - 2014",
+        companyLink: "https://www.kingshottschool.com/",
+          desc: "During my time in school, I was actively involved in a variety of activities and societies. I was a part of the School Council, which provided me with the opportunity to work closely with school administration and staff to plan and organize events and activities that benefit the student body. I was also a part of the Eco- Committee, which focused on promoting environmental awareness and sustainability within the school community.I participated in various initiatives such as organizing recycling programs, planting trees and creating awareness campaigns.Additionally, I was a member of the school's Hockey Team and the Chess Club. These activities allowed me to develop my teamwork, leadership and problem-solving skills. I was also part of the South Africa Exchange Program which gave me the opportunity to learn about different cultures, customs and ways of life, and also helped me to develop a global perspective!",
+    },
+  ],
+  resumeUrl:
+    "https://google.com",
+  socialLinks: {
+      instagram: "https://www.instagram.com/munns_aidan/",
+    twitter: "https://twitter.com",
+      linkedin: "https://www.linkedin.com/in/aidan-munns-359811139/",
+      github: "https://github.com/Woahai321",
+    facebook: "https://facebook.com/woahai",
+  },
+};
+
+export default userData;
